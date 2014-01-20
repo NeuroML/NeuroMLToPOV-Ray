f24310c030b1efe1a739437835f127665d4cc43b
Padraig Gleeson <p.gleeson@gmail.com>
2014-01-20 15:12:56 +0000

    Improved single cell example

--- ReadSim.py
+++ ReadSim.py
@@ -3,7 +3,7 @@ import os.path
 #
 #   A simple file for reading nC sims
 #
-#   Author: Padraig Gleeson
+#   Author: Padraig Gleeson & Matteo Farinella
 #
 #   This file has been developed as part of the neuroConstruct project
 #   This work has been funded by the Medical Research Council and Wellcome Trust
@@ -41,6 +41,11 @@ def process_args():
                         metavar='<minV>',
                         default=-90.0,
                         help='Min voltage for colour scale')
+
+    parser.add_argument('-singlecell',
+                        action='store_true',
+                        default=False,
+                        help="If this is specified, visualise activity in a single cell, with dat files for each segment")
 
     return parser.parse_args()
 
@@ -94,10 +99,15 @@ def main (argv):
             t=t+dt
 
         volts[cell_ref] = volt
+        print "Saved data for cell ref %s"%cell_ref
 
 
     t=0
     index = 0
+
+    #give the single frames an alphabetical order
+    maxind = "00000"
+    ind = "00000"
 
     bat_file_name = "%s_pov.bat"%(args.prefix)
     bat_file = open(bat_file_name, 'w')
@@ -107,60 +117,106 @@ def main (argv):
     
     while t<=maxTime:
         print "\n----  Exporting for time: %f  ----\n"%t
-        in_file = open(args.prefix+"_net.inc")
-        out_file_name = args.prefix+"_net.inc"+str(index)
-        out_file = open(out_file_name, 'w')
-        
-        for line in in_file:
-            if line.strip().startswith("//"):
-                ref = line.strip()[2:]
-                if volts.has_key(ref):
-                    vs = volts[ref]
-                    out_file.write("    "+vs[index]+"//"+ref+" t= "+str(t)+"\n")
+
+        if not args.singlecell:
+
+            in_file = open(args.prefix+"_net.inc")
+            out_file_name = args.prefix+"_net.inc"+str(index)
+            out_file = open(out_file_name, 'w')
+
+            for line in in_file:
+                if line.strip().startswith("//"):
+                    ref = line.strip()[2:]
+                    if volts.has_key(ref):
+                        vs = volts[ref]
+                        out_file.write("    "+vs[index]+"//"+ref+" t= "+str(t)+"\n")
+                    else:
+                        out_file.write("No ref: "+ref+"\n")
+
+
                 else:
-                    out_file.write("No ref: "+ref+"\n")
+                    out_file.write(line)
+
+            in_file.close()
+            out_file.close()
+            print "Written file: %s for time: %f"%(out_file_name, t)
+
+            in_file = open(args.prefix+".pov")
+            out_file_name = "%s_T%i.pov"%(args.prefix, index)
+            out_file = open(out_file_name, 'w')
+
+            clock = 0.5 * t/maxTime
+
+            pre = '%s_net.inc'%args.prefix
+            pre = pre.split('/')[-1]
+            post = '%s_net.inc%i'%(args.prefix,index)
+            post = post.split('/')[-1]
+
+            print "Swapping %s for %s"%(pre, post)
+
+            for line in in_file:
+                if line.find(pre)>=0:
+                    out_file.write(line.replace(pre,post))
+                else:
+                    out_file.write(line.replace("clock", str(clock)))
+
+            print "Written file: %s for time: %f"%(out_file_name, t)
+            in_file.close()
+            out_file.close()
+
+            toEx = os.path.realpath(out_file_name)
+
+            bat_file.write("C:\\Users\\Padraig\\AppData\\Local\\Programs\\POV-Ray\\v3.7\\bin\\pvengine.exe %s /nr /exit\n"%toEx)
+            sh_file.write("povray %s %s\n"%(povArgs,toEx) )
+
+        else:
+
+            ind = maxind[0:len(maxind)-len(str(index))] #compute index indentation
+
+            in_file = open(args.prefix+"_cells.inc")
+            out_file_name = args.prefix+"_cells.inc"+ind+str(index)
+            out_file = open(out_file_name, 'w')
+
+            for line in in_file:
+                if line.strip().startswith("//"):
+                    ref = line.strip()[2:]
+                    if volts.has_key(ref):
+                        vs = volts[ref]
+                        out_file.write("    "+vs[index]+"//"+ref+" t= "+ind+str(t)+"\n")
+                    else:
+                        out_file.write("//No ref: "+ref+"\n")
 
 
-            else:
-                out_file.write(line)
-            
-        in_file.close()
-        out_file.close()
-        print "Written file: %s for time: %f"%(out_file_name, t)
-        
-        in_file = open(args.prefix+".pov")
-        out_file_name = "%s_T%i.pov"%(args.prefix, index)
-        out_file = open(out_file_name, 'w')
-        
-        
-        clock = 0.5 * t/maxTime
+                else:
+                    out_file.write(line)
 
-        pre = '%s_net.inc'%args.prefix
-        pre = pre.split('/')[-1]
-        post = '%s_net.inc%i'%(args.prefix,index)
-        post = post.split('/')[-1]
+            in_file.close()
+            out_file.close()
+            print "Written file: %s for time: %f"%(out_file_name, t)
 
-        print "Swapping %s for %s"%(pre, post)
+            in_file = open(args.prefix+".pov")
+            out_file_name = "%s_T%s%i.pov"%(args.prefix, ind, index)
+            out_file = open(out_file_name, 'w')
 
-        for line in in_file:
-            if line.find(pre)>=0:
-                out_file.write(line.replace(pre,post))
-            else:
-                out_file.write(line.replace("clock", str(clock)))
 
-        print "Written file: %s for time: %f"%(out_file_name, t)
-        in_file.close()
-        out_file.close()
+            for line in in_file:
+                pre = '%s_cells.inc'%args.prefix
+                post = '%s_cells.inc%s%i'%(args.prefix, ind, index)
+                if line.find(pre)>=0:
+                    out_file.write(line.replace(pre,post))
+                else:
+                    clock = 0.1 * t/maxTime
+                    out_file.write(line.replace("clock", str(clock)))
 
-        toEx = os.path.realpath(out_file_name)
+            print "Written file: %s for time: %f"%(out_file_name, t)
+            in_file.close()
+            out_file.close()
 
-        bat_file.write("C:\\Users\\Padraig\\AppData\\Local\\Programs\\POV-Ray\\v3.7\\bin\\pvengine.exe %s /nr /exit\n"%toEx)
-        sh_file.write("povray %s %s\n"%(povArgs,toEx) )
+            toEx = os.path.realpath(out_file_name)
 
-        #subprocess.call(["C:\Users\Padraig\AppData\Local\Programs\POV-Ray\v3.7\bin\pvengine.exe", toEx , "/exit"])
-        #exit_code = os.waitpid(process.pid, 0)
-        #output = process.communicate()[0]
-        
+            bat_file.write("C:\\Users\\Padraig\\AppData\\Local\\Programs\\POV-Ray\\v3.7\\bin\\pvengine.exe %s /nr /exit\n"%toEx)
+            sh_file.write("povray %s %s\n"%(povArgs,toEx) )
+
         index=index+1
         t=t+stepTime
 
--- test_movie_cell.sh
+++ test_movie_cell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd SimulationDataCell
-python ../NeuroML2POVRay.py MyCellNet.nml1 -split
-python ../ReadSim_singleCell.py MyCellNet
+python ../NeuroML2POVRay.py MyCellNet.nml1 -split -scalex 1.5 -scalez 1.5  -background "<0,0,0,0.55>" 
+python ../ReadSim.py MyCellNet -singlecell
 chmod +x MyCellNet_pov.sh
 ./MyCellNet_pov.sh
 cd -
